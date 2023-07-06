a8881d31134bcbda29b6187ba5513f74e55712c6
Maxthegreat99 <100855415+Maxthegreat99@users.noreply.github.com>
2023-07-05 21:08:48 -0400

    ported to TShock 5.2 + others can now be Ghosted

--- Ghost2/Ghost2/Ghost.cs
+++ Ghost2/Ghost2/Ghost.cs
@@ -12,11 +12,11 @@ namespace Ghost
     {
         public override string Name => "Ghost";
 
-        public override string Author => "SirApples";
+        public override string Author => "SirApples, updated by moisterrific + Maxthegreat99";
 
         public override string Description => "A plugin that allows admins to become completely invisible to players.";
 
-        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+        public override Version Version => new Version(2, 1);
 
         public override void Initialize()
         {
@@ -24,18 +24,42 @@ namespace Ghost
         }
         void OnGhost(CommandArgs args)
         {
-            int i = Projectile.NewProjectile(args.Player.LastNetPosition.X, args.Player.LastNetPosition.Y, Vector2.Zero.X, Vector2.Zero.Y, 0, 0, 0, 16, 0, 0);
+
+            if (!args.Player.RealPlayer && args.Parameters.Count < 1)
+            {
+                args.Player.SendErrorMessage("You can't ghost the console!");
+                return;
+            }
+
+            Player TplayerAffected = args.TPlayer;
+            TSPlayer playerAffected = args.Player;
+            if (args.Parameters.Count > 0)
+            {
+                if (TSPlayer.FindByNameOrID(args.Parameters[0]).Count < 1)
+                {
+                    args.Player.SendErrorMessage("Player not found!");
+                    return;
+                }
+                TplayerAffected = TSPlayer.FindByNameOrID(args.Parameters[0])[0].TPlayer;
+                playerAffected = TSPlayer.FindByNameOrID(args.Parameters[0])[0];
+
+            }
+
+
+            int i = Projectile.NewProjectile(spawnSource: new Terraria.DataStructures.EntitySource_DebugCommand(), playerAffected.LastNetPosition.X, playerAffected.LastNetPosition.Y, Vector2.Zero.X, Vector2.Zero.Y, 0, 0, 0, 16, 0, 0);//Projectile.NewProjectile(args.Player.LastNetPosition.X, args.Player.LastNetPosition.Y, Vector2.Zero.X, Vector2.Zero.Y, 0, 0, 0, 16, 0, 0);
             Main.projectile[i].timeLeft = 0;
+
             NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, i);
-            args.TPlayer.active = !args.TPlayer.active;
-            NetMessage.SendData((int)PacketTypes.PlayerActive, -1, args.Player.Index, null, args.Player.Index, args.TPlayer.active.GetHashCode());
+            TplayerAffected.active = !TplayerAffected.active;
+            NetMessage.SendData((int)PacketTypes.PlayerActive, -1, playerAffected.Index, null, playerAffected.Index, TplayerAffected.active.GetHashCode());
 
             if (args.TPlayer.active)
             {
-                NetMessage.SendData((int)PacketTypes.PlayerInfo, -1, args.Player.Index, null, args.Player.Index);
-                NetMessage.SendData((int)PacketTypes.PlayerUpdate, -1, args.Player.Index, null, args.Player.Index);
+                NetMessage.SendData((int)PacketTypes.PlayerInfo, -1, playerAffected.Index, null, playerAffected.Index);
+                NetMessage.SendData((int)PacketTypes.PlayerUpdate, -1, playerAffected.Index, null, playerAffected.Index);
             }
-            args.Player.SendSuccessMessage($"{(args.TPlayer.active ? "Dis" : "En")}abled Ghost.");
+            playerAffected.SendSuccessMessage($"{(TplayerAffected.active ? "Dis" : "En")}abled Ghost.");
+            args.Player.SendSuccessMessage($"Player {playerAffected.Name} has now Ghost {(TplayerAffected.active ? "Dis" : "En")}abled.");
         }
 
         protected override void Dispose(bool disposing)
